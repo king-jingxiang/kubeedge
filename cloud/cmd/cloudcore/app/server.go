9106d39805e1359d08dba4e7c79b9e3677b02509
JingXiang <jingxiang@leinao.ai>
2021-12-07 16:38:46 +0800

    update

--- cloud/cmd/cloudcore/app/server.go
+++ cloud/cmd/cloudcore/app/server.go
@@ -143,7 +143,7 @@ func NegotiateTunnelPort() (*int, error) {
 	kubeClient := client.GetKubeClient()
 	err := httpserver.CreateNamespaceIfNeeded(kubeClient, constants.SystemNamespace)
 	if err != nil {
-		return nil, errors.New("failed to create system namespace")
+		return nil, errors.New("failed to create system namespace" + err.Error())
 	}
 
 	tunnelPort, err := kubeClient.CoreV1().ConfigMaps(constants.SystemNamespace).Get(context.TODO(), modules.TunnelPort, metav1.GetOptions{})
